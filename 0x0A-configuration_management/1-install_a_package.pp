95489375a9b5666ff59c314025636bbc53f9699f
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-11-25 00:42:15 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
-# non
-python::pip { 'Flask':
-  ensure     => '2.1.0',
-  pkgname    => 'Flask',
-  python     => 'python3',
-  require    => Class['python'],
+# 035 non
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip',
 }
