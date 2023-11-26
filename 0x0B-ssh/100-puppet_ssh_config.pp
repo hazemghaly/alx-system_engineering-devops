bfc27bae729132c11b8f2884d79984b079e6b824
hazemghaly <hazemghaly30@gmail.com>
2023-11-26 13:47:50 -0800

    SSH CONFIG

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+#Puppet
+file { '/home/ubuntu/.ssh/config':
+  ensure  => present,
+  content => @("SSH_CONFIG"
+    Host your_target_host
+      IdentityFile ~/.ssh/school
+      PasswordAuthentication no
+
+SSH_CONFIG
+  ),
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode    => '0600',
+}
