62649d059a27ed6be04dfaa60e4189608aa7fd81
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-11-28 18:35:14 +0200

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,17 +1,6 @@
 #Puppet
-<<<<<<< HEAD
-class ubuntu {
-    $public_key_content = file('/root/.ssh/school')
-   ssh_authorized_key { 'ubuntu@100.27.12.93':
-     ensure => present,
-     user   => 'ubuntu',
-     type   => 'ssh-rsa',
-     key    => $public_key_content,
-   }
-=======
 exec { 'ssh_authorized_key':
   cwd     => '/etc/ssh',
   creates => '/etc/ssh/ssh_config',
   path    => ['/root/.ssh/school.pub', '/root/.ssh/school'],
->>>>>>> 76d96b707543e05caff68ac83f046ca4b6f56584
 }
