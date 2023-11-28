5b8a76f17d22c807e1b8338c5c357b302b4d1c62
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-11-28 22:50:11 +0200

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,17 +3,19 @@
 class nginx_server {
   package { 'nginx':
     ensure => installed,
-  }
+}
 
-  service { 'nginx':
+service { 'nginx':
     ensure  => running,
     enable  => true,
     require => Package['nginx'],
-  }
+}
 
-  nginx::resource::server { 'example.com':
+nginx::resource::server { '${name}.${::default} ${name}':
     ensure      => present,
     listen_port => 80,
+    location              => '~ \.html$',
+    index_files           => ['index.html', 'index.htm'],
     location    => {
       '/' => {
         try_files => '$uri $uri/ =301',
