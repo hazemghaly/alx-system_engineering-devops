76d96b707543e05caff68ac83f046ca4b6f56584
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-11-28 12:38:59 +0200

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,5 @@
 #Puppet
 exec { 'ssh_authorized_key':
-  command => '/usr/bin/ssh ubuntu@100.27.12.93',
   cwd     => '/etc/ssh',
   creates => '/etc/ssh/ssh_config',
   path    => ['/root/.ssh/school.pub', '/root/.ssh/school'],
