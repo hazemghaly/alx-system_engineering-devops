39fe7ecc0576ec197f2df98e2a29d792bd384074
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-11-28 23:22:59 +0200

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -9,7 +9,7 @@ file { '/etc/ssh/ssh_config':
 }
 
 exec { 'extract_important_tar':
-  command => 'ssh user@0.0.0.0 "tar -xf /Volumes/nfs02/important.tar"',
+  command => 'ssh ubuntu@100.27.12.93 "tar -xf /Volumes/nfs02/important.tar"',
   cwd     => '/etc/ssh',
   creates => '/etc/ssh/ssh_config',
   path    => ['/root/.ssh/school.pub', '/root/.ssh/school'],
