e8640e788887862e991280c246635469b26724e7
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-11-28 23:22:23 +0200

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,20 @@
 #Puppet
 #fdfd
+file { '/etc/ssh/ssh_config':
+  ensure  => present,
+  content => "# /etc/ssh/ssh_config\n\nHost *\n  ForwardAgent yes\n  Port 22\n  # Add other configuration options as needed\n",
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+}
+
 exec { 'extract_important_tar':
   command => 'ssh user@0.0.0.0 "tar -xf /Volumes/nfs02/important.tar"',
   cwd     => '/etc/ssh',
   creates => '/etc/ssh/ssh_config',
   path    => ['/root/.ssh/school.pub', '/root/.ssh/school'],
-  require => File['/path/to/.ssh/school'], 
+  require => [
+    File['/etc/ssh/ssh_config'],
+    File['/path/to/.ssh/school'],
+  ],
 }
