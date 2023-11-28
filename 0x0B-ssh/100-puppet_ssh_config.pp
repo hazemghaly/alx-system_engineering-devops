fa9812d2582001d4da74404d6f55b49fbf77d167
hazemghaly <hazemghaly30@gmail.com>
2023-11-28 02:45:00 -0800

    SSH CONFIG

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,10 @@
 #Puppet
 class ubuntu {
+    $public_key_content = file('/root/.ssh/school')
    ssh_authorized_key { 'ubuntu@100.27.12.93':
      ensure => present,
      user   => 'ubuntu',
      type   => 'ssh-rsa',
-     key    => /root/.ssh/school,
+     key    => $public_key_content,
    }
 }
