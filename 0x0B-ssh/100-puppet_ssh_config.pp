63f91349a58456880363ecf6b09b84cc362c2aba
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-11-29 14:15:59 +0200

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -6,6 +6,12 @@ file { '/etc/ssh/ssh_config':
 
 file_line { 'no_pass':
   path  => ['/etc/ssh/ssh_config'],
-  line  => ['PasswordAuthentication no', 'IdentityFile ~/.ssh/school']
-  match => ['^#PasswordAuthentication', '^#IdentityFile']
+  line  => ['PasswordAuthentication no']
+  match => ['^#PasswordAuthentication']
+}
+
+file_line { 'Ident_file':
+  path  => ['/etc/ssh/ssh_config'],
+  line  => ['IdentityFile ~/.ssh/school']
+  match => ['^#IdentityFile']
 }
