9b206af959f09add433f14f0ae5da122f4d3bcf0
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-12-05 00:41:52 +0200

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,19 +4,17 @@ package { 'nginx':
   ensure => installed,
 }
 
-file_line { 'install':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-enabled/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-  line   => 'http {\n\tadd_header X-Served-By \"${hostname}\";',
-}
-
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
+file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
 }
 
 service { 'nginx':
   ensure  => running,
   require => Package['nginx'],
+}
+
+exec {'r':
+  command => '/usr/sbin/service nginx start',
 }
