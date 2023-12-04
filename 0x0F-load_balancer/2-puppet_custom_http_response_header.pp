d050b09ba0931efc4d4ff0b578117c379328e856
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-12-05 00:43:59 +0200

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,20 +1,40 @@
-#PUPET
-#file of PUPET
+# Install Nginx package
 package { 'nginx':
   ensure => installed,
 }
 
-file_line { 'http_header':
-  path  => '/etc/nginx/nginx.conf',
-  match => 'http {',
-  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+# Configure Nginx site
+nginx::resource::vhost { 'default':
+  ensure  => present,
+  content => '
+    listen      80 default_server;
+    server_name _;
+
+    location / {
+      add_header X-Served-By $hostname; # Add this line for X-Served-By header
+      root /var/www/html;
+      index index.html;
+    }
+
+    location /redirect_me {
+      rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+    }
+
+    error_page 404 /404.html;
+    location = /404.html {
+      internal;
+      root /var/www/html;
+    }
+  ',
 }
 
+# Create index.html
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+# Ensure Nginx service is running
 service { 'nginx':
   ensure  => running,
   require => Package['nginx'],
-}
-
-exec {'r':
-  command => '/usr/sbin/service nginx start',
 }
