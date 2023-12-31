d5c10d8ec30cab2182cfec7455edeb908400c098
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-12-31 23:40:50 +0200

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -13,8 +13,10 @@ file_line { 'install':
   ensure => 'present',
   path   => '/etc/nginx/sites-enabled/default',
   after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://github.com/hazemghaly permanent;',
-  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+  line   => [
+    'rewrite ^/redirect_me https://github.com/hazemghaly permanent;',
+    'http {\n\tadd_header X-Served-By "${hostname}";',
+  ],
 }
 
 file { '/var/www/html/index.html':
