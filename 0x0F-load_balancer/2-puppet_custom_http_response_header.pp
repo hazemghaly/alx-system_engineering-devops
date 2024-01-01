5f15b2268484f03966fe1664f87344cdb8bcd2f3
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-01-01 15:19:42 +0200

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,35 +1,16 @@
 # PUPPET
 # file of PUPPET
-exec { 'update':
-  command => '/usr/bin/apt-get update',
+class { 'nginx':
+  manage_repo => true,
 }
 
-package { 'nginx':
-  ensure => installed,
-}
-
-class { 'stdlib': }
-
-augeas { 'nginx_config':
-  context => '/files/etc/nginx/sites-enabled/default',
-  changes => [
-    'set listen[. = "80"]',
-    'set server[. = "listen 80 default_server;"]/rewrite',
-    'set server[. = "listen 80 default_server;"]/rewrite[1] ^/redirect_me https://github.com/hazemghaly permanent',
-    'set server[. = "listen 80 default_server;"]/http[. = "add_header X-Served-By \"${hostname}\";"]',
-  ],
-}
-
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
-}
-
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
-}
-
-exec { 'nginx_restart':
-  command => '/usr/sbin/service nginx restart',
-  subscribe => Augeas['nginx_config'],
+nginx::resource::server { 'example.com':
+  listen_port => '80',
+  server_name => 'example.com',
+  location    => {
+    '/' => {
+      'proxy_pass' => 'http://backend',
+      'proxy_set_header' => 'X-Served-By $hostname',
+    },
+  },
 }
