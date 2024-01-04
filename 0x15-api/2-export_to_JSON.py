16fc810d242a7ef128015a29dc948634f09279ee
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-01-04 02:27:36 +0200

    Update 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """
-A Script that, uses this REST API, for a given employee ID
-, returns list progress
+A sript that, uses a REST API, for a given employee ID, returns
+information about his/her TODO list progress
+and exports data in the JSON format.
 """
-import csv
+
 import json
 import requests
 from sys import argv
@@ -11,24 +12,30 @@ from sys import argv
 
 if __name__ == "__main__":
 
-    sessionEMP = requests.Session()
+    sessionReq = requests.Session()
 
-    ID = argv[1]
-    idURL = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(ID)
-    nameURL = 'https://jsonplaceholder.typicode.com/users/{}'.format(ID)
-    employee = sessionEMP.get(idURL)
-    name = sessionEMP.get(nameURL)
+    idEmp = argv[1]
+    idURL = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(idEmp)
+    nameURL = 'https://jsonplaceholder.typicode.com/users/{}'.format(idEmp)
+
+    employee = sessionReq.get(idURL)
+    employeeName = sessionReq.get(nameURL)
+
     json_req = employee.json()
-    EMPLOYEE_NAME = name.json()['name']
-    TOTAL_NUMBER_OF_TASKS = []
-    update = {}
-    for Emp in json_req:
-        TOTAL_NUMBER_OF_TASKS.append(
+    usr = employeeName.json()['username']
+
+    totalTasks = []
+    updateUser = {}
+
+    for all_Emp in json_req:
+        totalTasks.append(
             {
-                "task": Emp.get('title'),
-                "completed": Emp.get('completed'),
-                "username": EMPLOYEE_NAME,
+                "task": all_Emp.get('title'),
+                "completed": all_Emp.get('completed'),
+                "username": usr,
             })
-    update[ID] = TOTAL_NUMBER_OF_TASKS
-    with open(ID + ".json", 'w') as f:
-        json.dump(update, f)
+    updateUser[idEmp] = totalTasks
+
+    file_Json = idEmp + ".json"
+    with open(file_Json, 'w') as f:
+        json.dump(updateUser, f)
