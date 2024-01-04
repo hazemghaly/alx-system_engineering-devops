28b23d07ef5a936aca19bee162912b01a5ef788c
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-01-04 02:17:08 +0200

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -12,26 +12,20 @@ from sys import argv
 if __name__ == "__main__":
 
     sessionEMP = requests.Session()
-
     ID = argv[1]
     idURL = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(ID)
     nameURL = 'https://jsonplaceholder.typicode.com/users/{}'.format(ID)
-
     employee = sessionEMP.get(idURL)
     name = sessionEMP.get(nameURL)
-
     json_req = employee.json()
     EMPLOYEE_NAME = name.json()['name']
-
     TOTAL_NUMBER_OF_TASKS = 0
-
     for NUMBER_OF_DONE_TASKS in json_req:
         if NUMBER_OF_DONE_TASKS['completed']:
             TOTAL_NUMBER_OF_TASKS += 1
-
     # Example.csv gets created in the current working directory
     with open(ID + ".csv", "w", newline='') as csvfile:
-        my_writer = csv.writer(csvfile, delimiter=',')
+        my_writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
         for i in json_req:
             my_writer.writerow(
                 [ID, EMPLOYEE_NAME, i.get('completed'), i.get('title')])
