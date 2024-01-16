b3ad84e1e757f9b638381ab9610123cf117413ae
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-01-17 01:44:05 +0200

    Update 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+a function that queries the Reddit
+API and returns the number of subscribers
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """a function that queries the Reddit"""
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'YourAppName/1.0'}
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # errors check
+        data = response.json()
+        count = data['data']['subscribers']
+        return count
+    except Exception as e:
+        return (0)
