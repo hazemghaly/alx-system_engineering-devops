3c9a56cc5befc87ff329de7b81d0f9fcbe494488
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-01-17 02:20:41 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -16,6 +16,5 @@ def number_of_subscribers(subreddit):
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
-    data = response.json()
-    count = data.get('data', {}).get('subscribers', 0)
-    return count
+    results = response.json().get("data")
+    return results.get("subscribers")
