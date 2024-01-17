33a0dce9e1fc57faa61da5aaa4673b2caee45748
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-01-17 02:11:30 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,20 +10,15 @@ import requests
 def number_of_subscribers(subreddit):
     """a function that queries the Reddit"""
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'YourAppName/1.0'}
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        response.raise_for_status()  # errors check
-        if response.is_redirect:
-            return 0
-        try:
-            data = response.json()
-        except ValueError as e:
-            return 0
-        if 'data' in data and 'subscribers' in data['data']:
-            count = data['data']['subscribers']
-            return count
-        else:
-            return 0
-    except Exception as e:
-        return (0)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    data = response.json()
+    if 'data' in data and 'subscribers' in data['data']:
+        count = data['data']['subscribers']
+        return count
+    else:
+        return 0
