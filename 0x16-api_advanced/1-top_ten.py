cb0567b3a241feecb0e2963b1a9ba710336be6f5
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-01-17 03:38:31 +0200

    Update 1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+a function that queries the Reddit
+API and returns the number of subscribers
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """a function that queries the Reddit"""
+    url = f'https://www.reddit.com/r/{subreddit}/hot/.json'
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(
+        url, headers=headers, params=params, allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    data = response.json()
+    po = data.get("data").get("children")
+    for p in po:
+        title = p.get("data").get("title")
+        print(title)
