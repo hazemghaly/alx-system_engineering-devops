d6b276a068f8577f43d1f5bd6df4dec0b49293d6
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-01-24 01:10:34 +0200

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,5 @@
 #puppet
 exec { 'replace_php':
-    command => 'sed -i 's/ULIMIT="-n 15"/ULIMIT="-n 4096"/g' /etc/default/nginx',
-    command => 'sudo service nginx restart',
+    command => 'sed -i \'s/ULIMIT="-n 15"/ULIMIT="-n 4096"/g\' /etc/default/nginx && sudo service nginx restart',
     path    => ['/bin','/usr/bin']
 }
