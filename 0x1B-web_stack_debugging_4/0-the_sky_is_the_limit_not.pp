0ec8b8bbf6f63984373bad8de94ff9c4a35b92eb
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-01-24 13:17:25 +0200

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 #puppet
 exec { 'replace_limit':
-    command => 'sed -i \'s/ULIMIT="-n 15"/ULIMIT="-n 4096"/g\' /etc/default/nginx',
+    command => 'sed -i 's/15/4096/g' /etc/default/nginx',
     path    => ['/bin','/usr/bin']
 }
 
